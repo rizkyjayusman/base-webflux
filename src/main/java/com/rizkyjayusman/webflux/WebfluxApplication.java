eb5ca90cc1c494ca346c035a8e6d584b35c33bda
Rizky Jayusman <rizky.jayusman96@gmail.com>
2023-04-27 06:45:37 +0700

    add simple greeting endpoint

--- src/main/java/com/rizkyjayusman/webflux/WebfluxApplication.java
+++ src/main/java/com/rizkyjayusman/webflux/WebfluxApplication.java
@@ -2,12 +2,15 @@ package com.rizkyjayusman.webflux;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class WebfluxApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WebfluxApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(WebfluxApplication.class, args);
+		GreetingClient greetingClient = context.getBean(GreetingClient.class);
+		System.out.println(">> message = " + greetingClient.getMessage().block());
 	}
 
 }
